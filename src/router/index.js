7276cc0c40794cd3091b1a79c4e01a0248de4004
yb14672 <yb14672@gmail.com>
2022-11-09 16:09:23 +0800

    添加部门、字典前端页面

--- src/router/index.js
+++ src/router/index.js
@@ -8,26 +8,32 @@ import Home from '../components/Home.vue'
 import Person from "../components/user/Person"
 import Menu from "../components/system/Menu"
 import Role from "../components/system/Role"
+import Dict from "../components/system/dict/index"
+import DataType from "../components/system/dict/data"
+import Dept from '../components/system/Dept'
 
 Vue.use(Router)
 
 const router = new Router({
-  routes: [
-    {path: '/', name: 'Hello',component: Hello },
-    {path: '/login', component: Login },
-    {
-      path: '/home',
-      component: Home,
-      redirect: '/welcome',
-      children: [
-        { path: '/welcome', component: Welcome },
-        { path: '/system/user', component: Users },
-        { path: '/system/menu', component: Menu },
-        { path: '/system/role', component: Role },
-        {path: '/user/profile',component:Person},
-      ]
-    }
-  ]
+    routes: [
+        {path: '/', name: 'Hello', component: Hello},
+        {path: '/login', component: Login},
+        {
+            path: '/home',
+            component: Home,
+            redirect: '/welcome',
+            children: [
+                {path: '/welcome', component: Welcome},
+                {path: '/system/user', component: Users},
+                {path: '/system/menu', component: Menu},
+                {path: '/system/role', component: Role},
+                {path: '/user/profile', component: Person},
+                {path: '/system/dict', component: Dict},
+                {path: '/system/dept', component: Dept},
+                {path: '/dict/type/data/:dictId(\\d+)', component: DataType},
+            ]
+        }
+    ]
 })
 
 // //挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作
