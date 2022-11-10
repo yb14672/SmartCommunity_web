ef93fe63f6e99e82947c6c12b42c5f43fa72ee65
yb14672 <yb14672@gmail.com>
2022-11-10 23:22:46 +0800

    全部整合

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '../components/Welcome'
+import Hello from '../components/Hello'
 import Login from '../components/Login.vue'
 import Users from '../components/user/Users'
 import Home from '../components/Home.vue'
