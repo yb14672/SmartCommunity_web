3c597f9c3f5cf9dacdcd71faf91458c7e4f9fd66
ZHANGYOUWEI <1420803426@qq.com>
2022-11-10 20:14:53 +0800

    岗位管理

--- src/router/index.js
+++ src/router/index.js
@@ -8,27 +8,29 @@ import Person from "../components/user/Person"
 import Menu from "../components/system/Menu"
 import Role from "../components/system/Role"
 import Data from "../components/system/dict/index";
+import Post from "../components/system/Post";
 
 Vue.use(Router)
 
 const router = new Router({
-  routes: [
-    {path: '/', redirect: '/login'},
-    {path: '/login', component: Login },
-    {
-      path: '/home',
-      component: Home,
-      redirect: '/welcome',
-      children: [
-        { path: '/welcome', component: Welcome },
-        { path: '/system/user', component: Users },
-        { path: '/system/menu', component: Menu },
-        { path: '/system/role', component: Role },
-        {path: '/user/profile',component:Person},
-        {path:'/system/dict/',component:Data}
-      ]
-    }
-  ]
+    routes: [
+        {path: '/', redirect: '/login'},
+        {path: '/login', component: Login},
+        {
+            path: '/home',
+            component: Home,
+            redirect: '/welcome',
+            children: [
+                {path: '/welcome', component: Welcome},
+                {path: '/system/user', component: Users},
+                {path: '/system/menu', component: Menu},
+                {path: '/system/role', component: Role},
+                {path: '/user/profile', component: Person},
+                {path: '/system/dict/', component: Data},
+                {path:'/system/post',component:Post}
+            ]
+        }
+    ]
 })
 
 // //挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作
