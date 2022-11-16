9a1ab50e1097de4ed33b5112c45598c53f157d7d
lvweilin <18382187601@163.com>
2022-11-16 20:21:01 +0800

    操作日志删除清空

--- src/main.js
+++ src/main.js
@@ -56,6 +56,7 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 //接口前缀
 axios.defaults.baseURL = 'http://localhost:8080/'
+// axios.defaults.baseURL = 'http://49ac97eb.r3.cpolar.top/'
 //请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
 axios.interceptors.request.use(config=>{
   //为请求头对象，添加token验证的Authorization字段
